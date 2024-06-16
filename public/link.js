f89c6b2be45f7d7474a691f6afccfa98bac34a63
Matt Linder <linder2015@outlook.com>
2024-06-16 00:26:20 -0400

    adding redirection with data

--- public/link.js
+++ public/link.js
@@ -1,1 +1,5 @@
-window.location.replace("liftlogs://");
+// window.location.replace("liftlogs://");
+const query = window.location.search;
+const params = new URLSearchParams(query);
+const data = params.get("data");
+window.location.replace(`liftlogs://${data}`);
