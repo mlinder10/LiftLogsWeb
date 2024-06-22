15bb66673f7661be2137c91bfad25869b21faed4
Matt Linder <linder2015@outlook.com>
2024-06-22 00:27:11 -0400

    importing images instead of referencing by url

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,14 +1,16 @@
 import styles from "../styles/features.module.css";
+import exercise from "../assets/exercise.png";
+import planList from "../assets/planlist.png";
+import plan from "../assets/plan.png";
+import activeWorkout from "../assets/activeworkout.png";
+import charts from "../assets/charts.png";
+import summary from "../assets/summary.png";
 
 export default function Features() {
   return (
     <section className={styles.container}>
       <div className={styles["learn-container"]}>
-        <img
-          src="src/assets/exercise.png"
-          alt=""
-          className={styles["learn-img"]}
-        />
+        <img src={exercise} alt="" className={styles["learn-img"]} />
         <div className={styles["txt-container"]}>
           <p className={styles["section-title"]}>Learn New Exercises</p>
           <p className={styles["section-description"]}>Some description</p>
@@ -23,12 +25,12 @@ export default function Features() {
         <div className={styles["plan-img-container"]}>
           <img
             className={`${styles["plan-img"]} ${styles["plan-list"]}`}
-            src="src/assets/planlist.png"
+            src={planList}
             alt=""
           />
           <img
             className={`${styles["plan-img"]} ${styles["plan"]}`}
-            src="src/assets/plan.png"
+            src={plan}
             alt=""
           />
         </div>
@@ -37,9 +39,9 @@ export default function Features() {
       <div className={styles["track-container"]}>
         <p className={styles["track-title"]}>Track Your Progress</p>
         <div className={styles["img-container"]}>
-          <img src="src/assets/activeworkout.png" alt="" />
-          <img src="src/assets/charts.png" alt="" />
-          <img src="src/assets/summary.png" alt="" />
+          <img src={activeWorkout} alt="" />
+          <img src={charts} alt="" />
+          <img src={summary} alt="" />
         </div>
       </div>
     </section>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,10 @@
 import styles from "../styles/hero.module.css";
+import logo from "../assets/logo.jpeg";
 
 export default function Hero() {
   return (
     <section className={styles.container}>
-      <img src="src/assets/logo.jpeg" alt="" className={styles.logo} />
+      <img src={logo} alt="" className={styles.logo} />
       <div className={styles["txt-container"]}>
         <h1>Lift Logs</h1>
         <p>Conquer the gym</p>
