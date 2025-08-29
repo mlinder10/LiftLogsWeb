eb2819c42b2175837fc81e73118f824c753c6457
Matt Linder <mlinder@swampfox.com>
2025-08-29 18:20:11 -0400

    added auth pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const session = await getSession();
+  if (session) redirect("/");
+
+  return children;
+}
---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,11 @@
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function ProtectedLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const session = await getSession();
+  if (!session) redirect("/login");
+
+  return children;
+}
